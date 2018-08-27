5ced6fbd490dd9182d3d0124a0d3c12fb9442993
Tristan Handy <jthandy@gmail.com>
2018-08-27 10:03:53 -0400

    new stitch integration + structure for multiple adapters

--- macros/stitch/base/stitch_bing_accounts.sql
+++ macros/stitch/base/stitch_bing_accounts.sql
@@ -7,6 +7,29 @@
 
 {% macro default__stitch_bing_accounts() %}
 
-select 1
+select 
+
+    id as account_id,
+    number as account_number,
+    parentcustomerid as parent_customer_id,
+    paymentmethodid as payment_method_id,
+    primaryuserid as primary_user_id,
+    
+    name,
+    accounttype as account_type,
+    language,
+    timezone,
+    
+    accountfinancialstatus as account_financial_status,
+    accountlifecyclestatus as account_lifecycle_status,
+    billtocustomerid as bill_to_customer_id,
+    businessaddress as business_address,
+    countrycode as country_code,
+    currencytype as currency_type,
+    
+    lastmodifiedbyuserid as last_modified_by_user_id,
+    lastmodifiedtime as last_modified_time
+    
+from {{var('accounts_table')}}
 
 {% endmacro %}
