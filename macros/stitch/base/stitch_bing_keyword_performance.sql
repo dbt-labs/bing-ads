a777e87cf285fba102a1feec4295cfbd563a2be2
Alexa Diaz <axela.zaid@gmail.com>
2018-11-06 10:35:10 -0500

    added redshift adapter macro

--- macros/stitch/base/stitch_bing_keyword_performance.sql
+++ macros/stitch/base/stitch_bing_keyword_performance.sql
@@ -4,8 +4,85 @@
 
 {% endmacro %}
 
-
 {% macro default__stitch_bing_keyword_performance() %}
+
+with source as (
+
+    select * from {{var('keyword_performance_report_table')}}
+    
+),
+
+renamed as (
+
+    select
+    
+        "__SDC_PRIMARY_KEY" as keyword_performance_report_id,
+
+        convert_timezone('UTC', timeperiod)::timestamptz::date 
+            as campaign_date,
+            
+        accountid as account_id,
+        adgroupid as ad_group_id,
+        adgroupname as ad_group_name,
+        adid as ad_id,
+        campaignid as campaign_id,
+        campaignname as campaign_name,
+        keywordid as keyword_id,
+        
+        campaignstatus as campaign_status,
+        
+        coalesce(finalurl, '') as url,
+        
+        clicks,
+        impressions,
+        spend,
+        
+        rank() over (
+            partition by timeperiod::date 
+            order by _sdc_report_datetime desc
+            ) as rank
+
+    from source
+
+),
+
+parsed as (
+
+    select 
+    
+        keyword_performance_report_id,
+        campaign_date,
+        account_id,
+        ad_group_id,
+        ad_group_name,
+        ad_id,
+        campaign_id,
+        campaign_name,
+        campaign_status,
+        
+        url,
+        split_part(split_part(url, '//', 2), '/', 1)::varchar as url_host,
+        '/' || split_part(url, '.com/', 2)::varchar as url_path,
+        split_part(split_part(url,'utm_source=',2), '&', 1) as utm_source,
+        split_part(split_part(url,'utm_medium=',2), '&', 1) as utm_medium,
+        split_part(split_part(url,'utm_campaign=',2), '&', 1) as utm_campaign,
+        split_part(split_part(url,'utm_content=',2), '&', 1) as utm_content,
+        split_part(split_part(url,'utm_term=',2), '&', 1) as utm_term,
+        
+        clicks,
+        impressions,
+        spend
+        
+    from renamed
+    where rank = 1
+
+)
+
+select * from parsed
+
+{% endmacro %}
+
+{% macro snowflake__stitch_bing_keyword_performance() %}
 
 with source as (
 
